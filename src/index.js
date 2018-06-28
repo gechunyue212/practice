cbf8f4ea4ae94c39625b68ad06428d8c0cd79c1e
葛宝远 <yuansnowboy@163.com>
2018-06-28 19:28:52 +0800

    完成第二屏幕的动画

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ var mySwiper = new Swiper('.swiper-container', {
     watchSlidesProgress: true,
     pagination: {
         el: '.swiper-pagination',
-        clickable: true, // 可点击
+        clickable: true // 可点击
 
     },
     on: {
@@ -39,8 +39,192 @@ var mySwiper = new Swiper('.swiper-container', {
             swiperAnimate(this)
         },
         slideChangeTransitionEnd: function () {
+            switch (this.activeIndex) {
+                case 1:
+                    secondAnimateFn()
+                    break
+            }
             swiperAnimate(this)
+        },
+        slideChangeTransitionStart: function () {
+            function initSecondEls() {
+                var $svg = $('.swiper-slide2 svg'),
+
+                    svgBg = $svg.find('.svg-bg')[0],
+                    svgItem1 = $svg.find('.svg-item1')[0],
+                    svgItem2 = $svg.find('.svg-item2')[0],
+                    svgItem3 = $svg.find('.svg-item3')[0],
+                    svgText1 = $svg.find('.svg-text1')[0],
+                    svgText2 = $svg.find('.svg-text2')[0],
+                    svgText3 = $svg.find('.svg-text3')[0],
+                    line0 = $svg.find('.svg-line0')[0],
+                    line1 = $svg.find('.svg-line1')[0],
+                    line2 = $svg.find('.svg-line2')[0],
+                    line3 = $svg.find('.svg-line3')[0],
+                    line4 = $svg.find('.svg-line4')[0],
+                    line5 = $svg.find('.svg-line5')[0]
+                TweenMax.set(svgText1, {
+                    opacity: 0,
+                    scaleX: .3
+
+                })
+                TweenMax.set(svgText2, {
+                    opacity: 0,
+                    scaleX: .3
+
+                })
+                TweenMax.set(svgText3, {
+                    opacity: 0,
+                    scaleX: .3
+
+                })
+                TweenMax.set(svgBg, {
+                    opacity: 0,
+                    x: 280,
+                    y: 2000
+                })
+                TweenMax.set(svgItem1, {
+                    x: 324,
+                    y: 736
+                })
+                TweenMax.set(svgItem2, {
+                    y: 637,
+                    x: 1165
+                })
+                TweenMax.set(svgItem3, {
+                    x: 1038,
+                    y: 175
+                })
+                elInit(line0)
+                elInit(line1)
+                elInit(line2)
+                elInit(line3)
+                elInit(line4)
+                elInit(line5)
+                elInit(svgItem1)
+                elInit(svgItem2)
+                elInit(svgItem3)
+
+                function elInit(el) {
+                    TweenMax.set(el, {
+                        scale: '0,0',
+                        rotation: 100,
+                        transformOrigin: 'left bottom'
+                    })
+                }
+            }
+
+            switch (this.activeIndex) {
+                case 1:
+                    initSecondEls()
+                    break
+            }
         }
     }
 })
+
+
+// 第二屏的动画
+
+function secondAnimateFn() {
+    var secondAnimate = new TimelineMax(),
+        $svg = $('.swiper-slide2 svg'),
+        svgBg = $svg.find('.svg-bg')[0],
+        svgText1 = $svg.find('.svg-text1')[0],
+        svgText2 = $svg.find('.svg-text2')[0],
+        svgText3 = $svg.find('.svg-text3')[0],
+        svgItem1 = $svg.find('.svg-item1')[0],
+        svgItem2 = $svg.find('.svg-item2')[0],
+        svgItem3 = $svg.find('.svg-item3')[0],
+        line0 = $svg.find('.svg-line0')[0],
+        line1 = $svg.find('.svg-line1')[0],
+        line2 = $svg.find('.svg-line2')[0],
+        line3 = $svg.find('.svg-line3')[0],
+        line4 = $svg.find('.svg-line4')[0],
+        line5 = $svg.find('.svg-line5')[0]
+
+    secondAnimate
+        .to(svgBg, 1, {
+            x: 280,
+            y: 0,
+            opacity: 1
+
+        })
+        .to(line0, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            rotation: 0
+        })
+        .to(svgItem1, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            opacity: 1,
+            rotation: 0
+        })
+        .to(line1, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            rotation: 0
+        })
+        .to(svgText1, .543, {
+            opacity: 1,
+            scaleX: 1
+        })
+        .to(line1, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            rotation: 0
+        })
+        .to(line2, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            rotation: 0
+        })
+        .to(svgItem3, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            opacity: 1,
+            rotation: 0
+        })
+        .to(line3, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            rotation: 0
+        })
+        .to(svgText2, .543, {
+            opacity: 1,
+            scaleX: 1
+        })
+        .to(line4, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            rotation: 0
+        })
+        .to(svgItem2, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            opacity: 1,
+            rotation: 0
+        })
+        .to(line5, .543, {
+            transformOrigin: 'left bottom',
+            scaleY: 1,
+            scaleX: 1,
+            rotation: 0
+        })
+        .to(svgText3, .543, {
+            opacity: 1,
+            scaleX: 1
+        })
+
+}
 
